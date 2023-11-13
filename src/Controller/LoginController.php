60a98d32a60bc07637f014148aeb9b009b969dd2
BM-Y <belkermi.moubarek@gmail.com>
2023-11-13 18:20:32 +0100

    ajout login logout

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -7,11 +7,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+ 
 
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, ArticleRepository $articles): Response
+    public function login(AuthenticationUtils $authenticationUtils, ArticleRepository $article): Response
     {
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
@@ -20,7 +21,15 @@ class LoginController extends AbstractController
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
             'error'=> $error,
-            'articles'=> $articles,
+           
         ]);
     }
+    #[Route('/logout', name: 'app_logout', methods:['GET'])]
+    public function logout(): never 
+
+    {
+
+        throw new \Exception ('Don\t forget to activate logout in security.yaml');
+    }
+
 }
